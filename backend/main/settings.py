d7170e0b32188723de43e61979fd2b532b6267e4
Paul - Andrei Nafureanu <paul.nafureanu@gmail.com>
2022-08-31 00:33:38 +0300

    Add GoogleAPI backend app and Django REST Framework

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'GoogleAPI',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,11 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'apir',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'DevPassword'
     }
 }
 
