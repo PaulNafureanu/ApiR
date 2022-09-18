d280bc8039da2b3971fd6ae2f663530ac51fb983
PaulNafureanu <paul.nafureanu@gmail.com>
2022-09-19 00:26:53 +0300

    Backup for react tilt

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Home from "./components/home";
+import Start from "./components/start";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      {/* <Home /> */}
+      <Start/>
     </div>
   );
 }
